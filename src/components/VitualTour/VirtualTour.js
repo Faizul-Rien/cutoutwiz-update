78604b66a225ed0fe374e3ad9f94f55c6eaf23d7
shafayatC <shafayatplay@gmail.com>
2023-07-13 13:55:21 +0600

    update

--- src/components/VitualTour/VirtualTour.js
+++ src/components/VitualTour/VirtualTour.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 const VirtualTour = () => {
     return (
-        <div>
-            <iframe src="https://my.matterport.com/show/?m=M8e4FwAqnf4" width="770" height="510" frameborder="0" marginwidth="0" marginheight="0" scrolling="no"></iframe>
+        <div className='container'>
+            <div style={{ paddingTop:'30px'}}>
+                <iframe src="https://my.matterport.com/show/?m=M8e4FwAqnf4" width="100%" height="600px" frameborder="0" marginwidth="0" marginheight="0" scrolling="no"></iframe>
+            </div>
         </div>
     );
 };
