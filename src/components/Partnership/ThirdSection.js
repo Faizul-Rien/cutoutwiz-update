8a3704a018f06b3a4da39a1541582ee38f38d00c
faizulhassanRien <faizulhassan.rien@gmail.com>
2023-09-11 17:55:19 +0600

    mobile responsive

--- src/components/Partnership/ThirdSection.js
+++ src/components/Partnership/ThirdSection.js
@@ -5,20 +5,20 @@ const ThirdSection = () => {
     return (
 
         <div className='container'>
-        <div className='row ' style={{ marginTop: "110px", }}>
-            <div className='third-section-container'>
-                <div className='third-section-text'>
-                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in</p>
+            <div className='row '>
+                <div className='third-section-container'>
+                    <div className='third-section-text'>
+                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in</p>
+                    </div>
+                    <div className='third-section-photo-3'>
+                        <img src={photo3} />
+                    </div>
                 </div>
-                <div className='third-section-photo-3'>
-                    <img src={photo3} />
-                </div>
+
+
             </div>
 
-
         </div>
-
-    </div>
     );
 };
 
