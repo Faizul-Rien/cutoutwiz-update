80c60e98a4f85cb48e22fc671f37fd3384d04e61
faizulhassanRien <faizulhassan.rien@gmail.com>
2023-09-11 12:10:03 +0600

    add new partnership page

--- src/App.js
+++ src/App.js
@@ -63,12 +63,13 @@ import PDFViewer from "./components/PDFViewer/PDFViewer";
 import ThankUPdf from "./components/PDFViewer/ThankUPdf";
 import { FakeData } from "./components/FakeData/FakeData";
 import VirtualTour from "./components/VitualTour/VirtualTour";
+import Partnership from "./components/Partnership/Partnership";
 
-export const  ManageContext = createContext(); 
+export const ManageContext = createContext();
 
 export default function App() {
-  const [localData, setLocalData] = useState(FakeData); 
-  const [getUserLocation, setUserLocation] = useState(); 
+  const [localData, setLocalData] = useState(FakeData);
+  const [getUserLocation, setUserLocation] = useState();
   /*
   const userLocation = ()=> {
     fetch("https://ip-api.io/json?api_key="+process.env.REACT_APP_IPAPI)
@@ -105,220 +106,221 @@ export default function App() {
       
     })
   }*/
-  useEffect(()=>{
-   // userLocation()
-  },[])
+  useEffect(() => {
+    // userLocation()
+  }, [])
   return (
     <ManageContext.Provider value={[localData, getUserLocation, setUserLocation]}>
 
-    <div className="App">
-      <ScrollToTop/>
-     <Routes>
+      <div className="App">
+        <ScrollToTop />
+        <Routes>
           <Route path="/" element={<Navigation />}>
-          <Route index element={<Home />} />
-          <Route path="/service-portfolio" element={ <PDFViewer />} />
-          <Route path="/contact-us" element={ <Contact />} />
-          <Route path="/blog" element={ <Blog />} />
-          <Route path="/footer" element={ <Footer />} />
-          <Route path="/book-a-call" element={<Calendly />} />
-          <Route path="/thank-you-for-download" element={ <ThankUPdf />} />
+            <Route index element={<Home />} />
+            <Route path="/service-portfolio" element={<PDFViewer />} />
+            <Route path="/partnership" element={<Partnership />} />
+            <Route path="/contact-us" element={<Contact />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/footer" element={<Footer />} />
+            <Route path="/book-a-call" element={<Calendly />} />
+            <Route path="/thank-you-for-download" element={<ThankUPdf />} />
 
-          <Route
-            path="/5-important-photography-tips-for-beginners"
-            
-            element={ <Postphotography />}
-          />
-          <Route
-            path="/types-of-photography-which-one-drive-your-inner-passion-most"
-            
-            element={ <Typesofphoto />}
-          />
-          <Route
-            path="/explore-our-services" element={ <Test />}
-          />
-          <Route
-            path="/7-creative-winter-marketing-ideas-for-e-commerce-businesses"
-            
-            element={ <Wintermarketing />}
-          />
-          <Route
-            path="/easy-product-photography-solutions-for-small-business"
-            
-            element={ <Easyproductphoto />}
-          />
-          <Route
-            path="/top-shopify-marketing-strategy-for-sellers"
-            
-            element={ <Topshopify />}
-          />
-          <Route
-            path="/5-interesting-video-editing-facts"
-            
-            element={ <Videoediting />}
-          />
-          <Route
-            path="/minimize-your-bounce-rate"
-            
-            element={ <Minimizebounce />}
-          />
-          <Route
-            path="/how-to-make-a-softbox-for-product-photography"
-            
-            element={ <Softbox />}
-          />
-          <Route
-            path="/terms-and-conditions"
-            
-            element={ <Termsconditions />}
-          />
-          <Route
-            path="/privacy-policy"
-            
-            element={ <Privacypolicy />}
-          />
-          <Route
-            path="/top-new-year-resolution-for-your-online-store"
-            
-            element={ <Newyear />}
-          />
-          <Route
-            path="/product-photography-how-to-take-photos-for-your-e-commerce-business"
-            
-            element={ <Productphotography />}
-          />
-          <Route
-            path="/a-beginners-how-to-guide-for-clothing-photography"
-            
-            element={ <ClothingPhoto />}
-          />
-          <Route
-            path="/car-photography-a-beginners-how-to-guide-and-tips"
-            
-            element={ <CarPhoto />}
-          />
-          <Route
-            path="/product-reselling-photography-why-and-how-to-do-it"
-            
-            element={ <ProductReselling />}
-          />
-          <Route
-            path="/top-8-food-photography-tips-for-beginners"
-            
-            element={ <FoodPhoto />}
-          />
-          <Route
-            path="/influencer-marketing-how-to-find-the-right-one"
-            
-            element={ <InfluenceMarketing />}
-          />
-          <Route path="/thank-you/*" element={ <Thankyou />} />
-          <Route path="/all-blogs" element={ <AllBlogs />} />
-          <Route path="/demo-blog" element={ <Demoblog />} />
-          <Route
-            path="/how-to-turn-your-e-commerce-business-into-a-brand"
-            
-            element={ <EcomToBrand />}
-          />
-          <Route
-            path="/artificial-intelligence-is-used-in-image-editing"
-            
-            element={ <ArtInteli />}
-          />
-          <Route
-            path="/an-introduction-to-amazon-image-guidelines"
-            
-            element={ <AmazonImage />}
-          />
-          <Route
-            path="/photography-style-guide"
-            
-            element={ <PhotoStyle />}
-          />
-          <Route
-            path="/photography-style-guide2"
-            
-            element={ <PhotoStyle2 />}
-          />
-          <Route
-            path="/why-post-production-services-are-important-for-e-commerce-businesses"
-            
-            element={ <PostProduction />}
-          />
-          <Route
-            path="/why-you-should-outsource-your-image-editing"
-            
-            element={ <OutsourcePhoto />}
-          />
-          <Route
-            path="/srgb-colorspace-the-journey-from-introduction-to-importance"
-            
-            element={ <SrgbColorspace />}
-          />
-          <Route
-            path="/tips-for-reducing-bounce-rate-and-increasing-conversion"
-            
-            element={ <ReducingBounce />}
-          />
-          <Route
-            path="/why-is-graphic-design-important-for-e-commerce-business"
-            
-            element={ <GraphicsDesign />}
-          />
-          <Route
-            path="/importance-of-creative-image-editing-for-product-photos"
-            
-            element={ <CreativeImageEdit />}
-          />
-          <Route
-            path="/5-interesting-photo-editing-facts"
-            
-            element={ <PhotoFact />}
-          />
-          <Route path="/es" element={ <Spanish />} />
-          <Route path="/fr" element={ <France />} />
-          <Route path="/de" element={ <Germany />} />
-          <Route path="/nl" element={ <Dutch />} />
-          <Route path="/th" element={ <Thailand />} />
-          <Route
-            path="/es/thank-you/*"
-            
-            element={ <Thankyoues />}
-          />
-          <Route
-            path="/nl/thank-you/*"
-            
-            element={ <Thankyounl />}
-          />
-          <Route
-            path="/de/thank-you/*"
-            
-            element={ <Thankyoude />}
-          />
-          <Route
-            path="/fr/thank-you/*"
-            
-            element={ <Thankyoufr />}
-          />
-          <Route
-            path="/th/thank-you/*"
-            
-            element={ <Thankyouth />}
-          />
-          <Route path="/es/contact-us" element={ <Contactes />} />
-          <Route path="/nl/contact-us" element={ <Contactnl />} />
-          <Route path="/fr/contact-us" element={ <Contactfr />} />
-          <Route path="/de/contact-us" element={ <Contactde />} />
-          <Route path="/th/contact-us" element={ <Contactth />} />
-          <Route path="*" element={<NotFound/>} />
-          {/* Using path="*"" means "match anything", so this route
+            <Route
+              path="/5-important-photography-tips-for-beginners"
+
+              element={<Postphotography />}
+            />
+            <Route
+              path="/types-of-photography-which-one-drive-your-inner-passion-most"
+
+              element={<Typesofphoto />}
+            />
+            <Route
+              path="/explore-our-services" element={<Test />}
+            />
+            <Route
+              path="/7-creative-winter-marketing-ideas-for-e-commerce-businesses"
+
+              element={<Wintermarketing />}
+            />
+            <Route
+              path="/easy-product-photography-solutions-for-small-business"
+
+              element={<Easyproductphoto />}
+            />
+            <Route
+              path="/top-shopify-marketing-strategy-for-sellers"
+
+              element={<Topshopify />}
+            />
+            <Route
+              path="/5-interesting-video-editing-facts"
+
+              element={<Videoediting />}
+            />
+            <Route
+              path="/minimize-your-bounce-rate"
+
+              element={<Minimizebounce />}
+            />
+            <Route
+              path="/how-to-make-a-softbox-for-product-photography"
+
+              element={<Softbox />}
+            />
+            <Route
+              path="/terms-and-conditions"
+
+              element={<Termsconditions />}
+            />
+            <Route
+              path="/privacy-policy"
+
+              element={<Privacypolicy />}
+            />
+            <Route
+              path="/top-new-year-resolution-for-your-online-store"
+
+              element={<Newyear />}
+            />
+            <Route
+              path="/product-photography-how-to-take-photos-for-your-e-commerce-business"
+
+              element={<Productphotography />}
+            />
+            <Route
+              path="/a-beginners-how-to-guide-for-clothing-photography"
+
+              element={<ClothingPhoto />}
+            />
+            <Route
+              path="/car-photography-a-beginners-how-to-guide-and-tips"
+
+              element={<CarPhoto />}
+            />
+            <Route
+              path="/product-reselling-photography-why-and-how-to-do-it"
+
+              element={<ProductReselling />}
+            />
+            <Route
+              path="/top-8-food-photography-tips-for-beginners"
+
+              element={<FoodPhoto />}
+            />
+            <Route
+              path="/influencer-marketing-how-to-find-the-right-one"
+
+              element={<InfluenceMarketing />}
+            />
+            <Route path="/thank-you/*" element={<Thankyou />} />
+            <Route path="/all-blogs" element={<AllBlogs />} />
+            <Route path="/demo-blog" element={<Demoblog />} />
+            <Route
+              path="/how-to-turn-your-e-commerce-business-into-a-brand"
+
+              element={<EcomToBrand />}
+            />
+            <Route
+              path="/artificial-intelligence-is-used-in-image-editing"
+
+              element={<ArtInteli />}
+            />
+            <Route
+              path="/an-introduction-to-amazon-image-guidelines"
+
+              element={<AmazonImage />}
+            />
+            <Route
+              path="/photography-style-guide"
+
+              element={<PhotoStyle />}
+            />
+            <Route
+              path="/photography-style-guide2"
+
+              element={<PhotoStyle2 />}
+            />
+            <Route
+              path="/why-post-production-services-are-important-for-e-commerce-businesses"
+
+              element={<PostProduction />}
+            />
+            <Route
+              path="/why-you-should-outsource-your-image-editing"
+
+              element={<OutsourcePhoto />}
+            />
+            <Route
+              path="/srgb-colorspace-the-journey-from-introduction-to-importance"
+
+              element={<SrgbColorspace />}
+            />
+            <Route
+              path="/tips-for-reducing-bounce-rate-and-increasing-conversion"
+
+              element={<ReducingBounce />}
+            />
+            <Route
+              path="/why-is-graphic-design-important-for-e-commerce-business"
+
+              element={<GraphicsDesign />}
+            />
+            <Route
+              path="/importance-of-creative-image-editing-for-product-photos"
+
+              element={<CreativeImageEdit />}
+            />
+            <Route
+              path="/5-interesting-photo-editing-facts"
+
+              element={<PhotoFact />}
+            />
+            <Route path="/es" element={<Spanish />} />
+            <Route path="/fr" element={<France />} />
+            <Route path="/de" element={<Germany />} />
+            <Route path="/nl" element={<Dutch />} />
+            <Route path="/th" element={<Thailand />} />
+            <Route
+              path="/es/thank-you/*"
+
+              element={<Thankyoues />}
+            />
+            <Route
+              path="/nl/thank-you/*"
+
+              element={<Thankyounl />}
+            />
+            <Route
+              path="/de/thank-you/*"
+
+              element={<Thankyoude />}
+            />
+            <Route
+              path="/fr/thank-you/*"
+
+              element={<Thankyoufr />}
+            />
+            <Route
+              path="/th/thank-you/*"
+
+              element={<Thankyouth />}
+            />
+            <Route path="/es/contact-us" element={<Contactes />} />
+            <Route path="/nl/contact-us" element={<Contactnl />} />
+            <Route path="/fr/contact-us" element={<Contactfr />} />
+            <Route path="/de/contact-us" element={<Contactde />} />
+            <Route path="/th/contact-us" element={<Contactth />} />
+            <Route path="*" element={<NotFound />} />
+            {/* Using path="*"" means "match anything", so this route
                 acts like a catch-all for URLs that we don't have explicit
                 routes for. */}
-          <Route path="virtual-tour" element={<VirtualTour />} />
+            <Route path="virtual-tour" element={<VirtualTour />} />
 
-          <Route path="*" element={<NoMatch />} />
-        </Route>
-      </Routes>
-    </div>
+            <Route path="*" element={<NoMatch />} />
+          </Route>
+        </Routes>
+      </div>
 
     </ManageContext.Provider>
 
